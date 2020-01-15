2fdf48ca24a610ae7586811eb321e18a036d4050
Lucas Zanatta <lucas.zanatta@actionlabs.com.br>
2020-01-15 19:02:20 -0300

    Atualização

--- machine learning/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ machine learning/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -6,8 +6,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+np.set_printoptions(threshold = np.nan)
+
 #importing the datasets
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,3].values
+
+#take care of missing data
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy= 'mean', axis=0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+#encoding categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder();
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder();
+Y = labelencoder_Y.fit_transform(Y);
